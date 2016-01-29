910144b444630c0eec4caf202854bb47e935ac08
stivostenberg <megastiv@stiv.com>
2016-01-29 10:20:33 -0800

    Creating projects for the classes required to try to separate out the work from the UI and make a portable library for other projects.

---
+++ TrycorderScannerWS/AWSFunctions/AWSFunctions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AWSFunctions
+{
+    /// <summary>
+    /// Given credentials and arguments, retrieve data from AWS.
+    /// </summary>
+    public class ScanAWS
+    {
+    }
+
+    public class Credentials
+    {
+
+    }
+}
---
+++ TrycorderScannerWS/ScannerEngine/ScannerService.svc.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace ScannerEngine
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class ScannerClass : ScannerService
+    {
+        // This section will hold the private data the functions will interact with
+        //Accounts List with Credentials and an Enable/Disable field  Last success +  Fail Count.
+        //AWS Component Table [Enable, Last Complete Scan,  Last Incomplete,  ScanStatus]
+        //Scanner Status
+        //EC2 Table
+        //S3 Table
+        //IAM Table
+        //VPC table
+        //RDS Table
+
+
+
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+    }
+}
